bea6e8d79ea53a0fa4e097e522713fb661cd1c15
Guilherme <guilherme.schmitt@alkasoft.com.br>
2020-11-07 10:20:19 -0300

    Adicicionado typescript ao projeto, integração com o sqlite e migração para criar a tabela de orfanatos no banco

--- src/server.js
+++
@@ -1,8 +0,0 @@
-const { response } = require('express');
-const express = require('express');
-
-const app = express();
-
-app.get('/test', (req, res) => res.send("Hello world!"));
-
-app.listen(3333);
