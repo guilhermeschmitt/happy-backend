4f436bd70fc92d28f469ff95d140e7e2118f6a10
Guilherme <guilherme.schmitt@alkasoft.com.br>
2020-11-08 16:20:51 -0300

    Tratamento de requisição assíncronas no express e enviando uma mensagem de retorno genérica para o cliente

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import path from 'path';
+
 import express from 'express';
+import 'express-async-errors';
+
 import routes from './routes';
+import errorHandler from './errors/handler';
 
 import './database/connection';
 
@@ -9,5 +13,6 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use(errorHandler);
 
 app.listen(3333);
